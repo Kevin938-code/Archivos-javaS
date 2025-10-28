a56243a91456b290fad58fcecef0b10d0e8594b0
Kevin <kortizl5@unemi.edu.ec>
2025-10-27 21:22:19 -0500

    Add files via upload

---
+++ Guia-practica-2/Contar caracteres en palabras .js
@@ -0,0 +1,13 @@
+/*Contar caracteres en una palabra:
+Entrada: leer(palabra)
+Proceso:escribir ingrese una palabra
+leer palabra 
+Salida:escribir caracteres
+*/
+function caracteres(){
+    let palabra=""
+    palabra=prompt("Escribe una palabra")
+    alert(`La palabra tiene: ${palabra.length} letras.`)
+}
+debugger
+caracteres()
